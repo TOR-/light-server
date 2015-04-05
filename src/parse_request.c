45479f586fcfa758cdf89898260a9fbddc9c6df7
TOR- <TOR-@users.noreply.github.com>
2015-04-05 14:46:06 +0100

    Started Parsing GET requests

--- src/parse_request.c
+++ src/parse_request.c
@@ -1,6 +1,30 @@
 #include "light-server.h"
 
-int parse_request(char *buf)
+int parse_request(char buf[])
 {
+  // request type : only GET supported for now
+  switch(buf[0])
+  {
+    case 'G':
+    // 012345
+    // GET /file
+      if(buf[4]!='/')
+      {
+        return ERR_INVALID_FILE_PATH;
+      }
+      else
+      {
+        int i = 4;
+        char path[256];
+        for(;buf[i]!=' ';i++)
+        {
+          path[i-4] = buf[i];
+        }
+        if(path)
+        {
+          
+        }
+      }
+  }
   return 0;
 }
